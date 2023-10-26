def99680354671eb56bae9e9096291011e7557a5
Majo97 <88950572+Majo97@users.noreply.github.com>
2023-10-25 23:27:33 -0600

    :sparkles: initial commit

---
+++ proyecto_db/app/Http/Livewire/Administrador/PaginaPerfilAdministrador.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Administrador;
+
+use Livewire\Component;
+
+class PaginaPerfilAdministrador extends Component
+{
+    public function render()
+    {
+        return view('livewire.administrador.pagina-perfil-administrador');
+    }
+}
